8ef8195f8368251cc7040ed53fe9edf1794f91fb
Duy Vo <duy.vo@ptnglobalcorp.com>
2024-05-21 14:49:23 +0700

    Flutter lession 68

--- adv_basics/lib/questions_screen.dart
+++ adv_basics/lib/questions_screen.dart
@@ -1,3 +1,4 @@
+import 'package:adv_basics/answer_button.dart';
 import 'package:flutter/material.dart';
 
 class QuestionsScreen extends StatefulWidget {
@@ -7,7 +8,6 @@ class QuestionsScreen extends StatefulWidget {
   State<QuestionsScreen> createState() {
     return _QuestionsScreenState();
   }
-  
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
@@ -18,15 +18,19 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text('The question...'),
-          const SizedBox(height: 30,),
-          ElevatedButton(onPressed: (){}, child: const Text('Answer 1')),
-          ElevatedButton(onPressed: (){}, child: const Text('Answer 2')),
-          ElevatedButton(onPressed: (){}, child: const Text('Answer 3')),
-          ElevatedButton(onPressed: (){}, child: const Text('Answer 4')),
+          const Text(
+            'The question...',
+            style: TextStyle(color: Colors.white),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          AnswerButton(answerText: 'Answer 1', onTap: () {}),
+          AnswerButton(answerText: 'Answer 2', onTap: () {}),
+          AnswerButton(answerText: 'Answer 3', onTap: () {}),
+          AnswerButton(answerText: 'Answer 4', onTap: () {}),
         ],
       ),
     );
   }
-  
 }
