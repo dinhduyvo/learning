3d24cf20d0fca07ee836339c4aaf62a0cfc4b73a
Duy Vo <dinhduyvo@gmail.com>
2024-05-29 13:28:34 +0700

    Flutter Session 106

--- expense_tracker/lib/expenses.dart
+++ expense_tracker/lib/expenses.dart
@@ -25,13 +25,19 @@ class _ExpensesState extends State<Expenses> {
         category: Category.leisure),
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(context: context, builder: (ctx) {
+      return const Text('Model bottom sheet');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter Expense Tracker'),
         actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.add))
+          IconButton(onPressed: _openAddExpenseOverlay, icon: const Icon(Icons.add))
         ],
       ),
       body: Column(
