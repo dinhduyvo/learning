67023e6f117be5da9b5aeea2f2d2e42d0cdb4205
Duy Vo <duy.vo@ptnglobalcorp.com>
2024-05-31 15:24:16 +0700

    Flutter session 136

--- expense_tracker/lib/expenses.dart
+++ expense_tracker/lib/expenses.dart
@@ -63,6 +63,7 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+
     Widget mainContent = const Center(
       child: Text('No expense found. Start adding some!'),
     );
--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:expense_tracker/expenses.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+// import 'package:flutter/services.dart';
 
 var myColorScheme =
     ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
@@ -10,9 +10,9 @@ var myDarkColorScheme = ColorScheme.fromSeed(
     seedColor: const Color.fromARGB(255, 5, 99, 125));
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
-      .then((fn) {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+  //     .then((fn) {
     runApp(MaterialApp(
       darkTheme: ThemeData.dark(useMaterial3: true).copyWith(
         colorScheme: myDarkColorScheme,
@@ -46,5 +46,5 @@ void main() {
       themeMode: ThemeMode.system,
       home: const Expenses(),
     ));
-  });
+  // });
 }
