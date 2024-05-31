e886d538a211d19de0730d5d5a939f46eb7f97b8
Duy Vo <duy.vo@ptnglobalcorp.com>
2024-05-31 10:36:26 +0700

    Flutter session 129

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -4,8 +4,22 @@ import 'package:flutter/material.dart';
 var myColorScheme =
     ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
 
+var myDarkColorScheme = ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 5, 99, 125));
+
 void main() {
   runApp(MaterialApp(
+    darkTheme: ThemeData.dark(useMaterial3: true).copyWith(
+      colorScheme: myDarkColorScheme,
+      cardTheme: const CardTheme().copyWith(
+          color: myDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8)),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: myDarkColorScheme.primaryContainer,
+              foregroundColor: myDarkColorScheme.onPrimaryContainer)),
+    ),
     theme: ThemeData(
       useMaterial3: true,
     ).copyWith(
@@ -25,6 +39,7 @@ void main() {
                 fontWeight: FontWeight.bold,
                 color: myColorScheme.onSecondaryContainer,
                 fontSize: 16))),
+    themeMode: ThemeMode.dark,
     home: const Expenses(),
   ));
 }
