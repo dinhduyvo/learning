b174ef2e7522a9972c85fe494357942d66407442
Duy Vo <duy.vo@ptnglobalcorp.com>
2024-06-14 15:01:15 +0700

    Flutter session 161

--- meals/lib/main.dart
+++ meals/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/screens/categories.dart';
 import 'package:meals/screens/meals.dart';
 
 final theme = ThemeData(
@@ -24,7 +25,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: const MealsScreen(title: 'Some Category...', meals: dummyMeals,)
+      home: const CategoriesScreen()
     );
   }
 }
--- meals/lib/screens/categories.dart
+++ meals/lib/screens/categories.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(
+        builder: (ctx) => const MealsScreen(title: 'Some title', meals: [])));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,14 +20,15 @@ class CategoriesScreen extends StatelessWidget {
       body: GridView(
         padding: const EdgeInsets.all(24),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20
-        ),
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
         children: [
-         for (final category in availableCategories)
-          CategoryGridItem(category: category)
+          for (final category in availableCategories)
+            CategoryGridItem(category: category, onSelectCategory: () { 
+              _selectCategory(context);
+             },)
         ],
       ),
     );
